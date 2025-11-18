aa3a2f934cb3623a01822fe38d3bfff9fbf554b2
David Rockwood <daverockwood@gmail.com>
2025-11-18 15:23:18 -0700

    Remove help UI and related resources

--- tests/test_help_template.py
+++
@@ -1,7 +0,0 @@
-from fimpad.help import get_help_template
-
-
-def test_help_template_mentions_fim_tags():
-    template = get_help_template()
-    assert "[[[" in template
-    assert "[[[prefix]]]" in template or "prefix" in template.lower()
