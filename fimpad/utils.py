6fc21d9545fb2b1755b1aea4e971b0dff7da256a
David Rockwood <daverockwood@gmail.com>
2025-12-04 04:35:58 -0700

    Use ternary for offset column text

--- fimpad/utils.py
+++ fimpad/utils.py
@@ -11,10 +11,7 @@ def offset_to_tkindex(content: str, offset: int) -> str:
     prefix = content[:offset]
     line_no = prefix.count("\n") + 1
     last_newline = prefix.rfind("\n")
-    if last_newline == -1:
-        col_text = prefix
-    else:
-        col_text = prefix[last_newline + 1 :]
+    col_text = prefix if last_newline == -1 else prefix[last_newline + 1 :]
 
     col_units = len(col_text.encode("utf-16-le")) // 2
     return f"{line_no}.{col_units}"
